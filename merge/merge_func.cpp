b3c8ac351a19873a48c3eaf57ae5de65b3465737
bogerav <71005216+bogerav@users.noreply.github.com>
2021-04-03 13:48:41 +0300

    Create merge_func.cpp

---
+++ merge/merge_func.cpp
@@ -0,0 +1,27 @@
+//passed all tests
+Node* mergeLists(Node* first_node, Node* second_node) {
+	Node *new_node = new Node;
+	if (first_node == NULL or second_node == NULL) {
+		if (first_node == NULL and second_node == NULL)
+			return NULL;
+	if (first_node == NULL and second_node != NULL)
+		return second_node;
+	if (first_node != NULL and second_node == NULL)
+		return first_node;
+	} 
+  else {
+		if (first_node->value > second_node->value) {
+			new_node->value = second_node->value;
+			new_node->next = mergeLists(first_node, second_node->next); 
+		}
+		if (first_node->value < second_node->value) {
+			new_node->value = first_node->value;
+			new_node->next = mergeLists(first_node->next, second_node); 
+		}
+		if (first_node->value == second_node->value) {
+			new_node->value = first_node->value;
+			new_node->next = mergeLists(first_node->next, second_node->next);
+		}
+	}
+	return new_node;
+}
